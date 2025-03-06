e6cef7df29980de4d4c17fdfe488c1166ad7a3ee
CzarinaRamos <czar.ramos.co@gmail.com>
2025-03-06 15:08:35 -0800

    Added tests for transforming TextNodes into LeafNodes
    Also updated the TextType enum item name for normal text

--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 class TextType(Enum):
-    NORMAL = "normal text"
+    TEXT = "normal text"
     BOLD = "bold text"
     ITALIC = "italic text"
     CODE = "code text"
@@ -22,3 +22,4 @@ class TextNode():
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
 
+    
