e992bd6d3fbc41c2333068b3df7204d82191170c
CzarRamos <czar.ramos.co@gmail.com>
2025-03-10 15:55:20 -0700

    added HTMLnode and equivalent unit tests

---
+++ src/htmlnode.py
@@ -0,0 +1,20 @@
+
+
+class HTMLNode():
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def __eq__(self, other_htmlnode):
+        return self.tag == other_htmlnode.tag and self.value == other_htmlnode.value and self.children == other_htmlnode.children and self.props == other_htmlnode.props
+
+    def props_to_html(self):
+        output = ""
+        for item in self.props:
+            output += f" {item}=\"{self.props[item]}\""
+        return output
+
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
